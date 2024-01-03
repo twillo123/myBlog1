e8b2682be7d1f769effd9e6140e909637a8a1102
anushree05082001 <paridaanushree32@gmail.com>
2024-01-03 09:23:32 -0800

    127:create  save post feature

---
+++ src/main/java/com/myBlog/myBlog1/MyBlog1Application.java
@@ -0,0 +1,13 @@
+package com.myBlog.myBlog1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlog1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlog1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myBlog/myBlog1/MyBlog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBlog.myBlog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlog1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
